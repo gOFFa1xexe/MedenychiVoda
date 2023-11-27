5afe68c54f6e8a6dc14d8b48674cd4dd1b373b26
gOFFa1xexe <lol228gofa@gmail.com>
2023-11-27 13:01:15 +0200

    Start

---
+++ src/main/java/com/medvoda/medenychivoda/MedenychiVodaApplication.java
@@ -0,0 +1,13 @@
+package com.medvoda.medenychivoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedenychiVodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedenychiVodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medvoda/medenychivoda/MedenychiVodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medvoda.medenychivoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedenychiVodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
