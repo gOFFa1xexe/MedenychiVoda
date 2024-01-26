496a29b27681618b2393946fffe9481268c327ee
gOFFa1xexe <lol228gofa@gmail.com>
2024-01-26 13:11:47 +0200

    Today count added for every table

--- src/main/java/com/medvoda/medenychivoda/repositories/PackageRepository.java
+++ src/main/java/com/medvoda/medenychivoda/repositories/PackageRepository.java
@@ -1,12 +1,20 @@
 package com.medvoda.medenychivoda.repositories;
 
 import com.medvoda.medenychivoda.BottleEntity.CarbonationLevel;
+import com.medvoda.medenychivoda.BottleEntity.PackageCapacity;
 import com.medvoda.medenychivoda.BottleEntity.Packages;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public interface PackageRepository  extends JpaRepository<Packages,Long> {
-    int countByCarbonationLevel(CarbonationLevel carbonationLevel);
+    int countByCarbonationLevelAndPackageCapacity(CarbonationLevel carbonationLevel, PackageCapacity packageCapacity);
     void save(Package packageEntity);
 
+    int countByCarbonationLevelAndPackageCapacityAndCreatedAtAfter(CarbonationLevel carbonationLevel,
+                                                                   PackageCapacity packageCapacity,
+                                                                   LocalDateTime today);
+    int countByPackageCapacityAndCreatedAtAfter(PackageCapacity packageCapacity,LocalDateTime today);
     Packages findByCarbonationLevel(CarbonationLevel carbonationLevel);
 }
